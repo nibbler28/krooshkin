408d5b320c63252bb964b9c254a615ccbca87da3
nibbler28 <rodionov134@gmail.com>
2020-10-05 01:49:57 +0300

    Delete redundant webpack config

--- frontend-src/webpack.custom-prod.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const prod = require('./webpack.prod.js');
-
-module.exports = merge(prod, {
-  output: {
-     path: path.resolve(__dirname, 'customDist'),
-   },
-});
