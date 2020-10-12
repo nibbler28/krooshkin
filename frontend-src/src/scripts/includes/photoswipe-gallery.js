77a717435291df0b2b946ebea176206a6ec62c17
nibbler28 <rodionov134@gmail.com>
2020-10-13 02:50:03 +0300

    Add photoswipe titles, add viewport meta tag

--- frontend-src/src/scripts/includes/photoswipe-gallery.js
+++ frontend-src/src/scripts/includes/photoswipe-gallery.js
@@ -16,11 +16,13 @@ document.body.addEventListener('click', e => {
       const currentGridItemIndex = [...gridItems].findIndex(gridItem => gridItem === currentGridItem)
 
       const slideObjects = [...gridItems].map(gridItem => {
+        const img = gridItem.querySelector('img');
         return {
-          src: gridItem.querySelector('img').src,
+          src: img.src,
           w: gridItem.dataset.width,
           h: gridItem.dataset.height,
-          msrc: gridItem.querySelector('img').src
+          msrc: img.src,
+          title: img.dataset.title || ''
         }
       })
 
@@ -44,47 +46,3 @@ document.body.addEventListener('click', e => {
     }
   }
 })
-
-// const initializePhotoSwipe = () => {
-//   const grid = document.querySelector('.grid');
-//   let gridItems;
-//
-//   if (grid) {
-//     gridItems = document.querySelectorAll('.grid .item');
-//
-//     const slides = [...gridItems].map(gridItem => {
-//       return {
-//         src: gridItem.querySelector('img').src,
-//         w: 1200,
-//         h: 800,
-//         msrc: gridItem.querySelector('img').src
-//       }
-//     })
-//   }
-//
-//   document.body.addEventListener('click', e => {
-//     if (e.target.tagname === 'IMG' && e.target.closest('.item')) {
-//
-//       const item = e.target;
-//
-//       const itemsIndex = gridItems.findIndex(gridItem => gridItem === item);
-//
-//       const index = itemsIndex;
-//
-//       const options = {
-//         getThumbBoundsFn: function (index) {
-//           // See Options -> getThumbBoundsFn section of documentation for more info
-//           var thumbnail = gridItems[index].querySelector('img'), // find thumbnail
-//             pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-//             rect = thumbnail.getBoundingClientRect();
-//           console.log(rect)
-//
-//           return {x: rect.left, y: rect.top + pageYScroll, w: rect.width};
-//         }
-//       };
-//
-//       const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, slides, options);
-//       gallery.init();
-//     }
-//   })
-// }
